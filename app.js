f129e13a1a186137e8350195a5e69559fa5150d1
Leo <leonardokinouchi@gmail.com>
2024-11-26 16:10:34 -0300

    Deploy

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 3000
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://estoque-online.netlify.app/'
+}));
 
 (async () => {
   try {
