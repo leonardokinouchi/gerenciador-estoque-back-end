b34837cb077cec1306f8d1a5cda93ce09c1b7a7f
Leo <leonardokinouchi@gmail.com>
2024-11-26 15:44:21 -0300

    Deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const app = express()
 const cors = require('cors');
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 
@@ -32,6 +32,10 @@ const Produto = mongoose.model('Produto', {
   nome: String,
   quantidade: Number
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
 
 app.post('/produtos', async (req, res) => {
   const produto = new Produto({
